1a9d5d9c9c45ac3fcdedaa5c0e5278334560b177
Sridhar Ratnakumar <github@srid.name>
2015-02-08 15:05:39 -0800

    advance to interact with Org-mode

--- play.py
+++ play.py
@@ -1,19 +1,44 @@
 import rumps
 rumps.debug_mode(True)
 
-class AwesomeStatusBarApp(rumps.App):
-    @rumps.clicked("Preferences")
-    def prefs(self, _):
-        rumps.alert("jk! no preferences available!")
+def run(cmd):
+    import subprocess
+    return subprocess.check_output(cmd, shell=True).strip()
 
-    @rumps.clicked("Silly button")
-    def onoff(self, sender):
-        sender.state = not sender.state
+def run_emacsclient(elisp):
+    return run('emacsclient -e "%s"' % elisp)
 
-    @rumps.clicked("Say hi")
+def org_clock_string():
+    import re
+    if run_emacsclient("(org-clock-is-active)") == "nil":
+        return "Org IDLE"
+    else:
+        return run_emacsclient("(org-clock-get-clock-string)").splitlines()[0]
+
+def org_goto_clock():
+    import subprocess
+    cmd = 'emacsclient -e "(call-interactively \'org-clock-goto)"'
+    subprocess.check_call(cmd, shell=True)
+    subprocess.check_call(["open", "-a", "Emacs"]) # activate Emacs window
+
+class OrgClockStatusBarApp(rumps.App):
+    @rumps.clicked("Go to current task")
+    def current_task(self, _):
+        org_goto_clock()
+
+    @rumps.clicked("Refresh")
     def sayhi(self, _):
-        rumps.notification("Awesome title", "amazing subtitle", "hi!!1")
+        clock_title = org_clock_string()
+        self.title = clock_title
 
+        
 if __name__ == "__main__":
-    AwesomeStatusBarApp("Awesome App").run()
+    app = OrgClockStatusBarApp("Org clock")
+    def timer_func(sender):
+        print sender
+        app.title = org_clock_string()
+    timer = rumps.Timer(timer_func, 2)
+    timer.start()
+    app.run()
+    
     
